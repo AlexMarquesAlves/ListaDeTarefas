1f4741cf4215d1e1753a36b7b55e1e3d1ef015a2
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-10-10 18:35:31 -0300

    update

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -5,3 +5,16 @@ export const Container = styled.div`
   color: #797a81;
   min-height: 100vh;
 `;
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 2px;
+`;
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import * as C from './App.styles'
+import { Item } from './types/item'
+import { ListItem } from './components/ListItem';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'comprar pão na padaria!', done: false },
+    { id: 2, name: 'comprar um bolo na padaria!', done: false },
+  ]);
+
   return (
     <C.Container>
+      <C.Area>
+        <C.Header>LISTA DE TAREFAS</C.Header>
 
+        {/* Aréa de adicionar nova tarefa */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+
+      </C.Area>
     </C.Container>
   );
 }

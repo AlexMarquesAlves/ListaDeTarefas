84ca85b9ba769fc402a936c9ea6c023f4c8673b0
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-10-17 20:09:46 -0300

    finalizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
 import { useState } from 'react';
-import * as C from './App.styles'
-import { Item } from './types/item'
+import * as C from './App.styles';
+import { Item } from './types/item';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'comprar pão na padaria!', done: false },
-    { id: 2, name: 'comprar um bolo na padaria!', done: false },
+    // { id: 1, name: 'Comprar o pão na padaria', done: false },
+    // { id: 2, name: 'Comprar um bolo na padaria', done: true },
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  // Função feita para tarefinha de casa.
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
 
   return (
     <C.Container>
       <C.Area>
-        <C.Header>LISTA DE TAREFAS</C.Header>
+        <C.Header>Lista de Tarefas</C.Header>
 
-        {/* Aréa de adicionar nova tarefa */}
+        <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
-          <ListItem key={index} item={item} />
+          <ListItem
+            key={index}
+            item={item}
+            onChange={handleTaskChange}
+          />
         ))}
 
       </C.Area>
